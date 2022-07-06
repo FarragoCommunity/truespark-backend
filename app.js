c469cfd831b1a940b7e73e63f6e3820caa5d5cfa
FarragoCommunity <102537372+FarragoCommunity@users.noreply.github.com>
2022-07-06 18:40:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const adminRouter = require('./routers/adminRouter')
 // middleware
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000', credentials:true}));
+app.use(cors({origin:'*', credentials:true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"))
 app.use('/api/v1/users',userRouter)
