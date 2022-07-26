49c8e377155f12c46cf3e05df0b11e541813d69b
FarragoCommunity <102537372+FarragoCommunity@users.noreply.github.com>
2022-07-26 20:07:43 +0530

    new router added

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 const {verifyAdminToken} = require("../controllers/adminController");
 
 router.post('/',verifyAdminToken,userController.getUser);
+router.get('/:id',userController.findUserById)
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.post("/checkLoggedIn", userController.checkLoggedIn);
